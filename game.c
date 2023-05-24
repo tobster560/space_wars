f2a9b9e7b1d27a39e9d2d71cb0ce6092df79cb56
West <westt2@vdi-scclab-029.lancs.local>
2023-05-24 02:50:23 +0100

    removed attempts at gravity, there is now an endgame screen upon getting sucked, and the ship now warps if it goes out of bounds most bugs should of been fixed

--- game.c
+++ game.c
@@ -1,219 +1,129 @@
 #include <ncurses.h>  
 #include <unistd.h>
-<<<<<<< HEAD
 #include <time.h>
 #include <math.h>
 
-
- void accelerate (int rotation, float *verAcc, float *horAcc, float *fy, float *fx,int x,int y,WINDOW *game){
+// moves the spaceship by 0.5 'pixels' then prints the new position in the correct direction aswell as deleting the previous ship position
+ void accelerate (int rotation,float *fy, float *fx,int x,int y,WINDOW *game){
             
             if(rotation == 0){
-            *verAcc = *verAcc - 0.5;
            *fy = *fy - 0.5;
             mvwprintw(game,y,x,"/\\");
-            mvwprintw(game,y+1,x,"  "); 
+            mvwprintw(game,y+1,x,"  "); //
             }
             if(rotation == 90){
-            *fx = *fx + 0.5;
-            *horAcc = *horAcc + 0.5;
-            mvwprintw(game,y,x-1,"  ");
+            *fx = *fx + 0.5; 
+            mvwprintw(game,y,x-1,"  ");//
             mvwprintw(game,y,x,">");
-              
-           }
+           
+            }
             if(rotation == 180){
             *fy = *fy + 0.5;    
-            *verAcc = *verAcc + 0.5;
-            mvwprintw(game,y-1,x,"  ");
+            mvwprintw(game,y-1,x,"  "); //
             mvwprintw(game,y,x,"\\/");
-              
             }
             if(rotation == 270){
             *fx = *fx - 0.5;    
-            *horAcc = *horAcc - 0.5;
             mvwprintw(game,y,x+1,"  ");
             mvwprintw(game,y,x,"<");
-                     
             }
-          
-          
-    }
+        }
    
-  void gravity (float *verAcc, float *horAcc, float *fy, float *fx,WINDOW *game){
-
-    if (*fy > 11){
-        *verAcc = *verAcc - 0.5;
-    }else{
-        *verAcc = *verAcc + 0.5;
-    }
-       if (*fx > 40){
-        *horAcc = *horAcc - 0.5;
-    }else{
-        *horAcc = *horAcc + 0.5;
-    }
-    
-    mvwprintw(game,3,3,"x:%f,y:%f", *horAcc, *verAcc);
-  }
-
-=======
-
-
->>>>>>> refs/remotes/origin/master
 int main(){
 
-    initscr();
-    noecho();
-    cbreak();
-    curs_set(0);
-<<<<<<< HEAD
-    
+    initscr(); // initiates the screen
+    noecho(); // doesn't display user inputs on screen
+    cbreak(); //allows user to exit with control + c
+    curs_set(0);// hides the curses
+    //creates two windows, the one the game is played in and the one game over is displayed in
+    WINDOW *gameEnd = newwin(25,80,0,0);
     WINDOW *game = newwin(25,80,0,0);
+    
     refresh();
-    int x;
+    // defines the x and y values used for positioning the spaceship
+    int x;  
     int y;
-    int rotation = 90;
-  
-    float *horAcc;
-    float horAccs = 0;
-    horAcc = &horAccs;
-    float *verAcc;
-    float verAccs= 0;
-    verAcc = &verAccs;
-
-    float *fx;
+    int rotation; //stores direction ship is facing
+    // pointer to float points to float x value
+    float *fx; 
     float fxs = 5;
-    fx = &fxs;
+    fx = &fxs; 
+    // pointer to float points to float y value
     float *fy;
     float fys = 5;
     fy = &fys;
-    clock_t lastAccel = 0; // stores timestamp of last acceleration
-    clock_t lastGrav = 0; //stores timestampt of last gravity
-    int counter=0;
-    box(game,0,0);
+    
+    clock_t lastAccel = 0;   // stores the time since last acceleration
+    box(game,0,0);// creates a border
+    bool playing = true; //game will run while playing = true
     
     wrefresh(game);
     mvwprintw(game, 11,40, "*"); //prints the black hole
-    while (1){
-        time_t currentT = clock();
-        double elapsedSecondsGrav = (double)(currentT - lastGrav);
-       if (elapsedSecondsGrav >= 1000){
-        gravity(&verAccs, &horAccs, &fys ,&fxs,game);
-        lastGrav = currentT;
-        mvwprintw(game, 4,5 ,"gravity activated %d", counter);
-        counter = counter + 1;
-        } 
+    while (playing){
+        time_t currentTime = clock();
+        double elapsedSeconds = ( double)(currentTime -lastAccel); // stores the time since last accel in milliseconds
         char c = wgetch(game);
-
+        
+        // x/y =(int)fxs/fxy converts the float value to an int value so it can be used with mvwprintw
         if (c == 'w'){
             x = (int)fxs;
             y = (int)fys;
-            mvwprintw(game,y,x,"/\\");
+            mvwprintw(game,y,x,"/\\"); // displays the ship pointing upwards
             rotation = 0;
         }
         if (c == 'd'){
             x = (int)fxs;
             y = (int)fys;
-            mvwprintw(game,y,x,"  ");
-            mvwprintw(game,y,x,">");
+            mvwprintw(game,y,x,"  ");// clears the previous printing of the ship
+            mvwprintw(game,y,x,">"); // displays the ship pointing right
             rotation = 90;
          }
         if (c == 'a'){
             x = (int)fxs;
             y = (int)fys;
-            mvwprintw(game,y,x,"  ");
-            mvwprintw(game,y,x,"<");
+            mvwprintw(game,y,x,"  ");// clears the previous printing of the ship
+            mvwprintw(game,y,x,"<"); // displays the ship pointing left
             rotation = 270; 
         }
         if (c == 's'){
             x = (int)fxs;
             y = (int)fys;
-            mvwprintw(game,y,x,"\\/");
+            mvwprintw(game,y,x,"\\/"); // displays the ship pointing downwards
             rotation = 180;
         }
         
-        if (c == 'g'){
-            time_t currentTime = clock(); 
+        if (c == 'g' && elapsedSeconds >= 100){ // allows the user to accelerate every 0.1 seconds
             x = (int)fxs;
             y = (int)fys;
-            double elapsedSeconds = (double)(currentTime - lastAccel);
-            if (elapsedSeconds >= 1000){
-                mvwprintw(game,5,3,"%d",elapsedSeconds);
-            accelerate(rotation, &verAccs, &horAccs, &fys, &fxs, x, y, game);
-            lastAccel = currentTime;
-            }
-           
+            accelerate(rotation, &fys, &fxs, x, y, game); // moves the ship
+            lastAccel = currentTime; //resets the time since last acceleration
         
-            
-          
-
         }
-    }
         
-        if(x ==40 && y == 11) // ends game if cursor in the same place as black hole
+         if(x ==40 && y == 11) // ends game if cursor in the same place as black hole
         {
-             mvwprintw(game, 11,40, "game over"); 
+             mvwprintw(gameEnd, 1,1, "game over you got sucked by the black hole, press any key to exit"); // game over message
+             refresh();
+             wrefresh(gameEnd); // refreshes game end screen so it displays message
+             
+             playing = false; // ends the game loop
+        } 
+
+        // these if statements mean that if the ship goes out of bounds it warps back to the other end of the arena
+        if (fxs == 79){
+            fxs = 2;
         }
-       wrefresh(game);
-    
-    wrefresh(game);
-    endwin();
-    return 0;
+        if (fxs == 1){
+            fxs = 78;
+        }
+        if (fys ==24){
+            fys = 2;
+        }
+        if (fys == 2){
+            fys = 24;
+        }
     }
-    
-    
-
-=======
-
-    WINDOW *game = newwin(25,80,0,0);
-    refresh();
-    bool end = false;
-    int x =5;
-    int y =5;
-    int ch, prev_ch = 0;
-    keypad(game,true);
-    box(game,0,0);
-    
-   
-    wrefresh(game);
-    while (1){
-        char c = wgetch(game);
-       
-        if (c == 'w'){
-            mvwprintw(game,y,x,"/\\");
-        }
-        if (c == 'd'){
-            mvwprintw(game,y,x,"  ");
-            mvwprintw(game,y,x,">");
-        }
-        if (c == 'a'){
-            mvwprintw(game,y,x,"  ");
-            mvwprintw(game,y,x,"<");
-        }
-        if (c == 's'){
-            mvwprintw(game,y,x,"\\/");
-        }
-        
-        if (c == 'q') {
-            break; // exit loop if 'q' is pressed
-        }
-       
-       wrefresh(game);
-    }
-        
-
-
-    wrefresh(game);
-    if(end = true){
-          endwin();
-    } 
-    
-    
-
+    //ends the game after a keypress 
+    wgetch(gameEnd); 
     endwin();
-    
-    return 0;
-  
-}
->>>>>>> refs/remotes/origin/master
-
-    
-
+}  
